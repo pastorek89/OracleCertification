c37a0d128bf67563595234bd82bf76dcd652653c
pastorek89 <pastor.patryk@gmail.com>
2021-04-18 16:09:40 +0200

    Product Management project. Basis classes. Added rating possibility, reporting and formatting text,currency,date due localization.

---
+++ src/labs/pm/data/Product.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright (C) 2021 pastor
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package labs.pm.data;
+
+import java.math.BigDecimal;
+import static java.math.RoundingMode.HALF_UP;
+import java.time.LocalDate;
+import java.util.Objects;
+import static labs.pm.data.Rating.*;
+
+/**
+ * {@code Shop} represents class which manage Products
+ *
+ * @version 4.0
+ * @author pastor
+ */
+public abstract class Product implements Rateable<Product> {
+
+    public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
+    private int id;
+    private String name;
+    private BigDecimal price;
+    private Rating rating;
+
+    public Product(int id, String name, BigDecimal price, Rating rating) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+    }
+
+    public Product(int id, String name, BigDecimal price) {
+        this(id, name, price, NOT_RATED);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getDiscount() {
+        return price.multiply(DISCOUNT_RATE).setScale(2, HALF_UP);
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    @Override
+    public Rating getRating() {
+        return rating;
+    }
+
+    /**
+     * Get the value of bestBefore
+     *
+     * @return the value of bestBefore
+     */
+    public LocalDate getBestBefore() {
+        return LocalDate.now();
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "id = " + id + ", name = " + name + ", price = " + price + ", rating = " + rating.getStars() + " bestBefore = " + getBestBefore() + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 79 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Product other = (Product) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+}
